30697445c9f40ad6a9a9d5999b2d505d86b1e4bf
Md Shahbaz Alam <msalamiitd@gmail.com>
2023-11-18 16:40:05 +0530

    Add requirments

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from modular_code.Inference import Real_estate_inference
 
 st.title('Real Estate Price Prediction')
 
@@ -6,8 +7,8 @@ col1, col2, col3 = st.columns(3)
 
 
 with col1:
-    property_type = st.number_input('Property Type in BHK', min_value=1, max_value=6)
-    township_name = st.selectbox('Township name', options=['A','B'])
+    property_type = st.number_input('Property Type in BHK', min_value=1.0, max_value=6.0, step=0.5,format="%.1f")
+    township_name = st.selectbox('Township name', options=['Vanaha ', 'Godrej Hills retreat', 'ANP Universe', 'Urban Skyline','VTP Cierra', 'VTP Blue Water', 'OM Mangalam Chaitanya', 'Sukwani Aspire', 'Mantra 29 Gold', 'Mantra Monarch', 'Infinity', 'Mantra Insignia', '7Hills','Mantra Sky Tower', 'the work club', 'newton homes', 'lesiure Town', 'k ville', 'bluegrass residences', 'brookefield willows', 'tranquility annexe', 'mahalunge riviera ', 'simplicity', 'aureta ', 'unique legacy royale', 'Presidia ', 'Eternia 2.0', 'Bliss Solitaire', 'Impero', 'River Dale Residency', 'Neco Beaumont', 'Anutham' 'Sensorium ', 'kiarah terrazo', 'Dolphin Garima ', 'Oxy Beaumonde ', 'Sportsville ', 'Presidenta ', 'Bhaktamar Residency ', 'Parklane UrbanJoy ', 'khush vista', 'oxy eterno', 'Abitante Fiore', 'puraniks Sayama', 'Rhythm county ', 'Kalpataru Serenity', 'Satyam Shivam', 'Satyam Serenity', 'jade residences ', 'Satyam Shery', 'Atulya Kanhe', 'SUPREME ESTIA', 'TEJ ELEVIA', 'vtp solitaire '])
     mall_in = st.selectbox('Mall in township', options=['Yes', 'No'])
     gym_in = st.selectbox('Gym in township', options=['Yes', 'No'])
 
@@ -19,15 +20,20 @@ with col2:
 
 
 with col3:
-    property_locality = st.selectbox('Property locality', options=['A', 'B'])
+    property_locality = st.selectbox('Property locality', options=['Bavdhan', 'Mahalunge', 'Balewadi', 'Ravet', 'Baner', 'Kharadi','Koregaon Park', 'Keshav Nagar', 'KirkatWadi Sinhagad Road', 'Akurdi', 'pimpri pune', 'tathawade', 'hadapsar', 'kiwale', 'kayani nagar', 'pisoli', 'manjri ', 'mahalunge', 'handewadi', 'koregoan', 'Mundhwa', 'NIBM', 'BT Kawade RD', 'Undri ', 'Karvanagar ', 'magarpatta ', 'Hinjewadi ', 'ravet ', 'vimannagar', 'wadgaon sheri ', 'Susgaon ', 'mohammadwadi ', 'dhanori ','bavdhan budruk ', 'lonavala', 'baner', 'bavdhan', 'talegoan ', 'BANER'])
     school_university = st.selectbox('Is there is school/university near by', options=['Yes', 'No'])
     swimming_pool_in = st.selectbox('Swimming pool in property', options=['Yes', 'No'])
     state_name = st.selectbox('State', options=['Maharashtra/India'])
 
 
 property_description = st.text_input('Property Descriptions')
-value = {'Property type': property_type,'Town ship name': township_name,'Mall in Township':mall_in,'Gym in Property': gym_in, 'Property Area': property_area,'Club in Township': club_in, 'Park/Jogging Track': park_jogging_track, 'City name': city_name, 'Property Locality': property_locality, 'School//University': school_university, 'Swimming pool near by': swimming_pool_in,'State name': state_name}
+value = {'Propert Type': property_type,'TownShip Name/ Society Name': township_name,'Mall in TownShip':mall_in,'Gym': gym_in, 'Property Area in Sq. Ft.': property_area, 'ClubHouse': club_in, 'Park / Jogging track': park_jogging_track, 'City': city_name, 'Sub-Area': property_locality, 'School / University in Township': school_university, 'Swimming Pool': swimming_pool_in,'State': state_name, 'Description':property_description}
 st.write(value)
 st.write(property_description)
 
 submit_button = st.button(label='Prediction', type='primary')
+if submit_button:
+    real_state = Real_estate_inference(value)
+    lower, upper = real_state.predict()
+    text = 'The price of this property between {} lakhs and {} lakhs.'.format(lower, upper)
+    st.write(text)
--- modular_code/Inference.py
+++ modular_code/Inference.py
@@ -1,20 +1,59 @@
 # from ML_pipeline import load_model
 import nltk
+import re
+import pandas as pd
+import numpy as np
 
 
 class Real_estate_inference:
-    def __init__(self, data):
+    def __init__(self, test):
+        self.test_data = None
         self.model = None
-        self.data = data
+        self.test = test
+
+    def avg_property_area(self, x):
+        numbers = re.compile(r"[-+]?(\d*\.\d+|\d+)")
+        x = numbers.findall(x)
+        if len(x) == 1:
+            return np.float32(x[0])
+        elif len(x) == 2:
+            return (np.float32(x[0]) + np.float32(x[1])) / 2
+        else:
+            return -99
+
+    def processing(self, data):
+        numbers = re.compile(r"[-+]?(\d*\.\d+|\d+)")
+        data['Property Type clean'] = data['Propert Type'].apply(
+            lambda x: numbers.findall(x)[0] if len(numbers.findall(x)) > 0 else 0)
+        data['Sub-Area clean'] = data['Sub-Area'].apply(lambda x: x.lower().strip())
+        data['Company Name clean'] = data['Company Name'].apply(lambda x: x.lower().strip())
+        data['TownShip Name/ Society Name clean'] = data['TownShip Name/ Society Name'].apply(
+            lambda x: x.lower().strip())
+        data['Description clean'] = data['Description'].apply(lambda x: x.lower().strip())
+        data['ClubHouse Clean'] = data['ClubHouse'].apply(lambda x: x.lower().strip()).map(
+            {'yes': 1, 'no': 0})
+        data['School / University in Township Clean'] = data['School / University in Township '].apply(
+            lambda x: x.lower().strip()).map({'yes': 1, 'no': 0})
+        data['Hospital in TownShip Clean'] = data['Hospital in TownShip'].apply(
+            lambda x: x.lower().strip()).map({'yes': 1, 'no': 0})
+        data['Mall in TownShip Clean'] = data['Mall in TownShip'].apply(lambda x: x.lower().strip()).map(
+            {'yes': 1, 'no': 0})
+        data['Park / Jogging track Clean'] = data['Park / Jogging track'].apply(
+            lambda x: x.lower().strip()).map({'yes': 1, 'no': 0})
+        data['Swimming Pool Clean'] = data['Swimming Pool'].apply(lambda x: x.lower().strip()).map(
+            {'yes': 1, 'no': 0})
+        data['Gym Clean'] = data['Gym'].apply(lambda x: x.lower().strip()).map({'yes': 1, 'no': 0})
+        data['Property Area in Sq. Ft. clean'] = data['Property Area in Sq. Ft.'].apply(
+            lambda x: self.avg_property_area(str(x)))
+        numbers = re.compile(r"[-+]?(\d*\.\d+|\d+)")
+        data['Price in lakhs clean'] = data['Price in lakhs'].apply(
+            lambda x: np.float32(numbers.findall(str(x))[0]) if len(numbers.findall(str(x))) > 0 else np.nan)
+        print(data.head())
 
     def predict(self):
         # self.model = load_model()
-        pass
-
-
-def main():
-    nltk.download('stopwords')
-
-
-if __name__ == '__main__':
-    main()
+        self.test_data = pd.DataFrame(self.test, index=[0])
+        self.processing(self.test_data)
+        lower = 0
+        upper = 100
+        return lower, upper
